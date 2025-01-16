4c1477adc2440031e66e8982c2bdf532b00b4c3f
michaelgreen06 <michaelgreen06@gmail.com>
2025-01-16 18:10:50 +0700

    ended at 1hr & 13min

--- packages/plugin-devschool/src/index.ts
+++ packages/plugin-devschool/src/index.ts
@@ -6,7 +6,7 @@ export * as evaluators from "./evaluators/index.ts";
 export * as providers from "./providers/index.ts";
 
 export const devSchoolPlugin: Plugin = {
-    name: "devschool ",
+    name: "devschool",
     description: "Dev School example plugin",
     actions: [helloWorldAction],
 };
